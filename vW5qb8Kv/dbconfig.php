603118ee1f1d5a49b71cf2a35ea13a9890e393cc
seesselm <36243915+seesselm@users.noreply.github.com>
2018-11-17 18:48:55 -0500

    initial setup of database files

---
+++ vW5qb8Kv/dbconfig.php
@@ -0,0 +1,10 @@
+<?php
+$server="localhost";
+$username="seesselm_seessel";
+$pswd="Mattry01";
+$dbname="seesselm_ElectionTest";
+$table="volunteers";
+$table2="calllist";
+$table3="walklist";
+
+?>
---
+++ vW5qb8Kv/listusers.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<style>
+table, th, td {
+	border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+define("IN_CODE", 1);
+include ("dbconfig.php");
+
+$con = mysqli_connect($server, $username, $pswd, $dbname) or die("Connection fail");
+
+$query = "select id,name,email,password,walklist,calllist,district,authority from $dbname.$table";
+$result = mysqli_query($con, $query);
+echo "<table><tr>
+<th>id</th>
+<th>name</th>
+<th>email</th>
+<th>password</th>
+<th>walklist</th>
+<th>calllist</th>
+<th>district</th>
+<th>authority</th>
+</tr>";
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>
+<td>" . $row["id"] . "</td>
+<td>" . $row["name"] . "</td>
+<td>" . $row["email"] . "</td>
+<td>" . $row["password"] . "</td>
+<td>" . $row["walklist"] . "</td>
+<td>" . $row["calllist"] . "</td>
+<td>" . $row["district"] . "</td>
+<td>" . $row["authority"] . "</td>
+</tr>";
+}
+echo "</table>";
+mysqli_close($con);
+mysqli_free_result($result);
+?>
